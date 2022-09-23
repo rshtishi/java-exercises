661bd38f6536e948f191dabde38dfe19da75ed33
rshtishi <rando.shtishi@gmail.com>
2022-09-23 22:57:28 +0200

    exercise 14

---
+++ src/main/java/com/github/rshtishi/Exercise14.java
@@ -0,0 +1,41 @@
+package com.github.rshtishi;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Exercise14 {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int[] array = random.ints(100000, 1, 100).toArray();
+        int count = 0;
+
+        //Arrays.stream(array).forEach(e -> System.out.print(e + " "));
+        System.out.println();
+        System.out.println();
+        int[] uniqueArr = Arrays.stream(array)
+                .boxed()
+                .collect(Collectors.toSet())
+                .stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+
+        Arrays.stream(uniqueArr).forEach(e -> System.out.print(e + " "));
+
+        System.out.println();
+        System.out.println();
+
+        Map<Integer, Integer> map = Arrays.stream(array).boxed().collect(Collectors.toMap(Function.identity(), v -> 1, Integer::sum));
+        System.out.println(map);
+
+        System.out.println();
+        System.out.println();
+
+        map.entrySet().stream().sorted( (e1,e2)->e2.getValue().compareTo(e1.getValue())).forEach( e->System.out.print(e+" "));
+    }
+}
