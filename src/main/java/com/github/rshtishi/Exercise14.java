449722ed90c98d446cea8157ee3225ee24c2c94b
rshtishi <rando.shtishi@gmail.com>
2022-11-21 21:29:56 +0100

    fixed exercise 14

--- src/main/java/com/github/rshtishi/Exercise14.java
+++ src/main/java/com/github/rshtishi/Exercise14.java
@@ -1,9 +1,6 @@
 package com.github.rshtishi;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -12,30 +9,40 @@ public class Exercise14 {
     public static void main(String[] args) {
 
         Random random = new Random();
-        int[] array = random.ints(100000, 1, 100).toArray();
+        int[] array = random.ints(100, 1, 100).toArray();
         int count = 0;
 
-        //Arrays.stream(array).forEach(e -> System.out.print(e + " "));
-        System.out.println();
-        System.out.println();
-        int[] uniqueArr = Arrays.stream(array)
-                .boxed()
-                .collect(Collectors.toSet())
-                .stream()
-                .mapToInt(Integer::intValue)
-                .toArray();
+        Map<Integer, Integer> map = Arrays.stream(array) //instream
+                .boxed() // Stream<Integer>
+                .collect(Collectors.toMap(key -> key, v -> 1, (a, b) -> a + b));
 
-        Arrays.stream(uniqueArr).forEach(e -> System.out.print(e + " "));
-
-        System.out.println();
-        System.out.println();
-
-        Map<Integer, Integer> map = Arrays.stream(array).boxed().collect(Collectors.toMap(key->key, v -> 1, (a,b) ->a+b));
         System.out.println(map);
-
         System.out.println();
         System.out.println();
 
-        map.entrySet().stream().sorted( (e1,e2)->e2.getValue().compareTo(e1.getValue())).forEach( e->System.out.print(e+" "));
+        List<Integer> uniqueEl = map.entrySet().stream()
+                .filter(entry -> entry.getValue() == 1)
+                .map(entry -> entry.getKey())
+                .collect(Collectors.toList());
+
+        System.out.println(uniqueEl);
+        System.out.println();
+        System.out.println();
+
+        List<Integer> duplicates = map.entrySet().stream()
+                .filter(entry -> entry.getValue() > 1)
+                .map(entry -> entry.getKey())
+                .collect(Collectors.toList());
+
+        System.out.println(duplicates);
+        System.out.println();
+        System.out.println();
+
+        List<Integer> sortedList = map.entrySet().stream()
+                .sorted((e1, e2) -> Integer.compare(e2.getValue(), e1.getValue()))
+                .map(e -> e.getKey())
+                .collect(Collectors.toList());
+
+        sortedList.stream().limit(25).forEach(System.out::println);
     }
 }
