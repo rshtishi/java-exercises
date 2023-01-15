6d222057263e05f8f16b6dd1f0c509b27fbc9f05
rshtishi <rando.shtishi@gmail.com>
2023-01-15 10:40:13 +0100

    exercise 4, ready work

--- src/main/java/com/github/rshtishi/Exercise4.java
+++ src/main/java/com/github/rshtishi/Exercise4.java
@@ -16,38 +16,53 @@ public class Exercise4 {
         }
 
         public void addToStorage(String key, String value) {
-            List<String> values = data.get(key);
-            if (values == null) {
-                values = new ArrayList<>();
-            }
-            values.add(value);
-            data.put(key,values);
+            /**
+             * 1. Check if key Exists in map
+             * 1.a.1 if exists then get List corresponding to the key
+             * 1.a.2 add value to the List
+             * 1.b.1 if doesnt exit create a new list
+             * 1.b.2 add value to the list
+             * 1.b.3 and key and list in map
+             */
         }
 
         public void printValues(String key) {
-            data.get(key).stream()
-                    .forEach(System.out::println);
+
+            /**
+             * 1. convert map to entryset
+             * 2. convert entryset to stream
+             * 3. flatmap {java ->[John,Jim,Jane]} -> John, Jim, Jane
+             * 4. foreach print
+             */
         }
 
         public void findValues(String value) {
-            data.entrySet().stream().filter(entry -> entry.getValue().contains(value))
-                    .map(entry -> entry.getKey())
-                    .forEach(System.out::println);
-        }
-    }
+            /**
+             * 1. convert map -> entryset
+             * 2. convert entrySet -> stream
+             * 3. filter by value
+             * 4. map entry to string , e.g. {java ->[John,Jim,Jane]} -> java
+             * 5. foreach print
+             */
 
-    public static void main(String[] args) {
 
-        Storage storage = new Storage();
-        storage.addToStorage("java","John");
-        storage.addToStorage("java","Gerard");
-        storage.addToStorage("java","Jane");
-        storage.addToStorage("java","Mary");
-        storage.addToStorage("javascript","John");
-        storage.addToStorage("javascript","Jane");
+ }
+ }
 
-        storage.printValues("javascript");
+ public static void main(String[] args) {
 
-        storage.findValues("John");
-    }
-}
+ Storage storage = new Storage();
+ storage.addToStorage("java","John");
+ storage.addToStorage("java","Gerard");
+ storage.addToStorage("java","Jane");
+ storage.addToStorage("java","Mary");
+ storage.addToStorage("javascript","John");
+ storage.addToStorage("javascript","Jane");
+
+ storage.printValues("javascript");
+
+ storage.findValues("John");
+ }
+ }
+
+ }
