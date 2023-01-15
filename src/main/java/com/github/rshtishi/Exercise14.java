28a9a4eeb151020228ded2763df510b4c329c707
rshtishi <rando.shtishi@gmail.com>
2023-01-15 13:52:51 +0100

    added exercise 14

--- src/main/java/com/github/rshtishi/Exercise14.java
+++ src/main/java/com/github/rshtishi/Exercise14.java
@@ -9,40 +9,61 @@ public class Exercise14 {
     public static void main(String[] args) {
 
         Random random = new Random();
-        int[] array = random.ints(100, 1, 100).toArray();
+        int[] array = random.ints(100, 0, 100).toArray();
         int count = 0;
 
-        Map<Integer, Integer> map = Arrays.stream(array) //instream
-                .boxed() // Stream<Integer>
-                .collect(Collectors.toMap(key -> key, v -> 1, (a, b) -> a + b));
+        Map<Integer,Integer> valuesByFreqMap = generateValueByFreqMap(array);
 
-        System.out.println(map);
-        System.out.println();
-        System.out.println();
+        List<Integer> uniqueValues = generateUniqueValuesList(valuesByFreqMap);
 
-        List<Integer> uniqueEl = map.entrySet().stream()
-                .filter(entry -> entry.getValue() == 1)
-                .map(entry -> entry.getKey())
-                .collect(Collectors.toList());
+        List<Integer> duplicates = generateDuplicatesValuesList(valuesByFreqMap);
 
-        System.out.println(uniqueEl);
-        System.out.println();
-        System.out.println();
+        List<Integer> mostFrequentNumberList = generateMostFrequentNumberList(valuesByFreqMap)
 
-        List<Integer> duplicates = map.entrySet().stream()
-                .filter(entry -> entry.getValue() > 1)
-                .map(entry -> entry.getKey())
-                .collect(Collectors.toList());
+    }
 
-        System.out.println(duplicates);
-        System.out.println();
-        System.out.println();
+    private static List<Integer> generateMostFrequentNumberList(Map<Integer, Integer> valuesByFreqMap) {
+        /**
+         * convert map to entrySet
+         * convert entrySet to stream
+         * sort stream comparator
+         * limit 25
+         */
+        return null;
+    }
 
-        List<Integer> sortedList = map.entrySet().stream()
-                .sorted((e1, e2) -> Integer.compare(e2.getValue(), e1.getValue()))
-                .map(e -> e.getKey())
-                .collect(Collectors.toList());
+    private static List<Integer> generateDuplicatesValuesList(Map<Integer, Integer> valuesByFreqMap) {
+        /**
+         * TO DO
+         *  convert map to entrySet
+         *  convert entrySet to stream
+         *  filter (entry) -> entry.getValue()>1
+         *  map entry -> entry.getKey()
+         *  convert stream to List
+         */
+        return null;
+    }
 
-        sortedList.stream().limit(25).forEach(System.out::println);
+    private static List<Integer> generateUniqueValuesList(Map<Integer, Integer> valuesByFreqMap) {
+        /**
+         * TO DO
+         *  convert map to entrySet
+         *  convert entrySet to stream
+         *  filter (entry) -> entry.getValue()==1
+         *  map entry -> entry.getKey()
+         *  convert stream to List
+         */
+        return null;
+    }
+
+    private static Map<Integer, Integer> generateValueByFreqMap(int[] array) {
+        /**
+         * 1. If map contains key (number of array)
+         * 1.a.1 (True) get value for key
+         * 1.a.2 increment 1
+         * 1.a.3 add to map
+         * 1.b.1 (False) add to map key me value 1
+         */
+        return null;
     }
 }
