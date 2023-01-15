6b83357ff1c2ec05214fdbcb914084d31f7bcf18
rshtishi <rando.shtishi@gmail.com>
2023-01-15 09:20:30 +0100

    exericse 3 - ready to work

--- src/main/java/com/github/rshtishi/Exercise3.java
+++ src/main/java/com/github/rshtishi/Exercise3.java
@@ -12,21 +12,16 @@ public class Exercise3 {
         addressMap.put("City", "Tirana");
         addressMap.put("Street", "Blv Gjergj Fishta");
         addressMap.put("Building", "15");
-
+        System.out.println(addressMap);
         printMap(addressMap);
 
     }
 
     private static void printMap(Map<String, String> map) {
-        List<String> list = map.keySet().stream().map(key -> prepareText(key, map)).
-                collect(Collectors.toList());
-        String text = String.join(",\n", list) + ".";
-        System.out.println(text);
+
     }
 
     private static String prepareText(String key, Map<String, String> map) {
-        StringBuilder stringBuilder = new StringBuilder("Key:");
-        stringBuilder.append(key).append(", Value:").append(map.get(key));
-        return stringBuilder.toString();
+        return null;
     }
 }
