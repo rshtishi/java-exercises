be13e5b553ae9622020109d3cb2b94e240083c38
rshtishi <rando.shtishi@gmail.com>
2023-02-26 15:02:45 +0100

    exercise2 test

--- src/main/java/com/github/rshtishi/Exercise2Test.java
+++ src/main/java/com/github/rshtishi/Exercise2Test.java
@@ -2,14 +2,22 @@ package com.github.rshtishi;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Exercise2Test {
 
     public static List<String> sortIgnoreCase(List<String> list){
-        // TO DO
-        // Sort list
-        return list;
+        Comparator<String> sortAscIgnoreCase = (s1,s2) -> s1.compareToIgnoreCase(s2) ;
+        Comparator<String> sortDescIgnoreCase = sortAscIgnoreCase.reversed();
+
+        return list.stream().sorted(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareToIgnoreCase(o1);
+            }
+        }).collect(Collectors.toList());
     }
 
     public static void main(String[] args){
