dc380bf48f802ea06efd8f150e83a4f4e71a44cf
rshtishi <rando.shtishi@gmail.com>
2023-03-04 15:10:57 +0100

    added exercise 9

--- src/main/java/com/github/rshtishi/Exercise9Test.java
+++ src/main/java/com/github/rshtishi/Exercise9Test.java
@@ -3,16 +3,69 @@ package com.github.rshtishi;
 public class Exercise9Test {
 
     public static class Point2D {
-        // TO DO
+        private double x;
+        private double y;
 
+        public Point2D(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public void setX(double x) {
+            this.x = x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public void setY(double y) {
+            this.y = y;
+        }
     }
 
     public static class Circle {
-        // TO DO
+
+        private static final double PI = 3.14;
+        private Point2D center;
+        private Point2D point;
+
+        public Circle(Point2D center, Point2D point) {
+            this.center = center;
+            this.point = point;
+        }
+
+        private double calculateRadius() {
+            double dx2 = Math.pow((center.getX() - point.getX()), 2);
+            double dy2 = Math.pow((center.getY() - point.getY()), 2);
+            double radius = Math.sqrt(dx2 + dy2);
+            return radius;
+        }
+
+        public double getRadius() {
+            return calculateRadius();
+        }
+
+        public double getPerimeter() {
+            return 2 * PI * calculateRadius();
+        }
+
+        public double getArea() {
+            double radius = calculateRadius();
+            return PI * Math.pow(radius, 2);
+        }
     }
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        Circle circle = new Circle(new Point2D(1, 1), new Point2D(2, 2));
+        System.out.println(circle.getRadius());
+        System.out.println(circle.getPerimeter());
+        System.out.println(circle.getArea());
     }
 }
