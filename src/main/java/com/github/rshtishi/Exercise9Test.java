03638549f3f3c9471f71370d7bb76c086fadfb1c
rshtishi <rando.shtishi@gmail.com>
2023-03-04 14:31:04 +0100

    added exercise 9

---
+++ src/main/java/com/github/rshtishi/Exercise9Test.java
@@ -0,0 +1,18 @@
+package com.github.rshtishi;
+
+public class Exercise9Test {
+
+    public class Point2D {
+        // TO DO
+
+    }
+
+    public class Circle {
+        // TO DO
+    }
+
+
+    public static void main(String[] args){
+
+    }
+}
