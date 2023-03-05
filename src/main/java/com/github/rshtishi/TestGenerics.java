b7e3f8bd0feb12a47e40b9ef54b1e418a7241c24
rshtishi <rando.shtishi@gmail.com>
2023-03-05 12:35:32 +0100

    test

--- src/main/java/com/github/rshtishi/TestGenerics.java
+++ src/main/java/com/github/rshtishi/TestGenerics.java
@@ -15,7 +15,19 @@ public class TestGenerics {
 
     public static void main(String[] args){
 
+        //1 Way
         Supplier<String> stringSuplier = new SupplierImpl();
         System.out.println(stringSuplier.get());
+
+        //2 WAY
+        Supplier<Double> doubleSupplier = new Supplier<Double>() {
+            @Override
+            public Double get() {
+                return 10.2;
+            }
+        };
+        System.out.println(doubleSupplier.get());
+
+
     }
 }
