5d456b1c23f985541280f3e135c58e340af5bdf4
rshtishi <rando.shtishi@gmail.com>
2023-03-05 15:06:39 +0100

    test generics

--- src/main/java/com/github/rshtishi/TestGenerics.java
+++ src/main/java/com/github/rshtishi/TestGenerics.java
@@ -40,43 +40,43 @@ public class TestGenerics {
         //Built in Functional interfaces
 
         //Return a random string
-        Supplier<String> supplierEx = null;
+        Supplier<String> supplierEx = () -> "Supplier";
         System.out.println(supplierEx.get());
 
         //print string
-        Consumer<String> consumer = null;
+        Consumer<String> consumer = (String s) -> System.out.println(s);
         consumer.accept("Hello World");
 
         //print string format, Country:Albania,Population:3000000
-        BiConsumer<String, Integer> biConsumer = null;
+        BiConsumer<String, Integer> biConsumer = (String country, Integer population) -> System.out.println(String.format("Country:%s,Population:%d", country, population));
         biConsumer.accept("Albania ", 3000000);
 
         //check if string is empty
-        Predicate<String> predicate = null;
+        Predicate<String> predicate = (String s) -> s.isEmpty();
         System.out.println(predicate.test("Rando"));
 
         //check if string is empty and integer is positive
-        BiPredicate<String, Integer> biPredicate = null;
+        BiPredicate<String, Integer> biPredicate = (String s, Integer i)->s.isEmpty() && i>0;
         System.out.println(biPredicate.test("test", 10));
 
-        //return the character of the string passsed as input
-        Function<String, Integer> function = null;
+        //return the number of character of the string passsed as input
+        Function<String, Integer> function = (String s)-> s.length();
         System.out.println(function.apply("Rando"));
 
         //return the string, Name: Rando, Age:30
-        BiFunction<String, Integer, String> biFunction = null;
+        BiFunction<String, Integer, String> biFunction = (String s, Integer i)->String.format("Name: %s, Age:%d",s,i);
         System.out.println(biFunction.apply("Rando", 30));
 
         //increase number with 1
-        UnaryOperator<Integer> unaryOperator = null;
+        UnaryOperator<Integer> unaryOperator = (Integer i)->i++;
         System.out.println(unaryOperator.apply(1));
 
         //add two number and return the results
-        BinaryOperator<Integer> binaryOperator = null;
+        BinaryOperator<Integer> binaryOperator = (Integer a, Integer b)-> a+b;
         System.out.println(binaryOperator.apply(4, 2));
 
         //compare the strings
-        Comparator<String> comparator = null;
+        Comparator<String> comparator = (String str1, String str2) -> str1.compareTo(str2);
         System.out.println(comparator.compare("John", "Jim"));
 
 
