c0df93ad443391e2777d2399b60efc509a72206d
rshtishi <rando.shtishi@gmail.com>
2023-03-05 14:18:54 +0100

    test generics

--- src/main/java/com/github/rshtishi/TestGenerics.java
+++ src/main/java/com/github/rshtishi/TestGenerics.java
@@ -1,11 +1,12 @@
 package com.github.rshtishi;
 
-import java.util.function.Supplier;
+import java.util.Comparator;
+import java.util.function.*;
 
 public class TestGenerics {
 
 
-    public static class SupplierImpl implements Supplier<String>{
+    public static class SupplierImpl implements Supplier<String> {
 
         @Override
         public String get() {
@@ -13,7 +14,7 @@ public class TestGenerics {
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         //1 Way
         Supplier<String> stringSuplier = new SupplierImpl();
@@ -30,9 +31,54 @@ public class TestGenerics {
 
         // 3 WAY
         Supplier<String> s1 = () -> "A";
-        Supplier<String> s2 = () -> { return "A";};
+        Supplier<String> s2 = () -> {
+            return "A";
+        };
         System.out.println(s1.get());
         System.out.println(s2.get());
+
+        //Built in Functional interfaces
+
+        //Return a random string
+        Supplier<String> supplierEx = null;
+        System.out.println(supplierEx.get());
+
+        //print string
+        Consumer<String> consumer = null;
+        consumer.accept("Hello World");
+
+        //print string format, Country:Albania,Population:3000000
+        BiConsumer<String, Integer> biConsumer = null;
+        biConsumer.accept("Albania ", 3000000);
+
+        //check if string is empty
+        Predicate<String> predicate = null;
+        System.out.println(predicate.test("Rando"));
+
+        //check if string is empty and integer is positive
+        BiPredicate<String, Integer> biPredicate = null;
+        System.out.println(biPredicate.test("test", 10));
+
+        //return the character of the string passsed as input
+        Function<String, Integer> function = null;
+        System.out.println(function.apply("Rando"));
+
+        //return the string, Name: Rando, Age:30
+        BiFunction<String, Integer, String> biFunction = null;
+        System.out.println(biFunction.apply("Rando", 30);
+
+        //increase number with 1
+        UnaryOperator<Integer> unaryOperator = null;
+        System.out.println(unaryOperator.apply(1));
+
+        //add two number and return the results
+        BinaryOperator<Integer> binaryOperator = null;
+        System.out.println(binaryOperator.apply(4, 2));
+
+        //compare the strings
+        Comparator<String> comparator = null;
+        System.out.println(comparator.compare("John", "Jim"));
+
 
     }
 }
