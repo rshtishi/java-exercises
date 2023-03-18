3492beb81ba8b500e165cc94f2744b89af074bc0
rshtishi <rando.shtishi@gmail.com>
2023-03-18 12:23:25 +0100

    implelmented example

--- src/main/java/com/github/rshtishi/TestList.java
+++ src/main/java/com/github/rshtishi/TestList.java
@@ -1,34 +1,37 @@
 package com.github.rshtishi;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class TestList {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        List<String>  list = new ArrayList<>();
+        List<String> list = new ArrayList<>();
         init(list);// initilize list with 5 names
         print(list);// print all names in the list
+        System.out.println();
         printEvenIndexNames(list);//print all names in that are in even index
 
     }
 
     private static void printEvenIndexNames(List<String> list) {
-        //TO DO
-        //print even index names
-        //use String.format
+        for(int i =0;i<list.size();i++){
+            if(i%2==0){
+                System.out.println(String.format("Name: %s",list.get(i)));
+            }
+        }
     }
 
     private static void print(List<String> list) {
-        // TO DO
-        //print all names in format Name: X
-        //use String.format
+        for(String el:list){
+            System.out.println(String.format("Name: %s",el));
+        }
     }
 
     private static void init(List<String> list) {
-        //TO DO
-        //initilize list with 5 names
+        list.addAll(Arrays.asList("Jim", "Jane", "John", "Stuart", "Michael"));
     }
 }
